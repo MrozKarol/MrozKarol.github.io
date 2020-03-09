ce81486b925237025966845881f738f3695236d7
MrozKarol <polityk357@gmail.com>
2020-03-09 18:05:10 +0100

    added text typing function

--- JS/main.js
+++ JS/main.js
@@ -1,29 +1,35 @@
 
-const spnText = document.querySelector('span');
-const spnCoursor = document.querySelector('.cursor');
-const time = 100
-const text = 'Cześć !, strona w przebudowie wróć proszę za kilka dni ;) pozdrawiam Karol'
+const textTyping = () => {
+    const spnText = document.querySelector('span');
+    const spnCoursor = document.querySelector('.cursor');
+    const time = 100
+    const text = 'Cześć !, strona w przebudowie wróć proszę za kilka dni ;) pozdrawiam Karol'
 
-let indexText = 0;
-const addLetter = () => {
-    spnText.textContent += text[indexText];
-    indexText++
-    if (indexText === text.length) {
-        clearInterval(indexTyping)
+    let indexText = 0;
+    const addLetter = () => {
+        spnText.textContent += text[indexText];
+        indexText++
+        if (indexText === text.length) {
+            clearInterval(indexTyping)
+        }
+        // console.log(indexText)
     }
-    // console.log(indexText)
+
+    const coursorAnimation = () => {
+        spnCoursor.classList.toggle('active');
+        if (indexText === text.length) {
+            spnCoursor.classList.remove('active')
+            clearInterval(coursor)
+
+        }
+
+    }
+
+    const indexTyping = setInterval(addLetter, time);
+
+    const coursor = setInterval(coursorAnimation, 200)
 }
 
-const coursorAnimation = () => {
-    spnCoursor.classList.toggle('active');
-    if (indexText === text.length) {
-        spnCoursor.classList.remove('active')
-        clearInterval(coursor)
 
-    }
-    
-}
 
-const indexTyping = setInterval(addLetter, time);
-
-const coursor = setInterval(coursorAnimation, 200)
+textTyping()
